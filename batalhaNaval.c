a89100748efcaf87d77f93d53f5eee3e34ee5b35
Vagner do Carmo <vluzrmos@gmail.com>
2026-04-05 20:16:03 -0300

    Nivel aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -6,6 +6,8 @@ int AGUA=0;
 int NAVIO=3;
 int TAMANHO_NAVIO_1=3;
 int TAMANHO_NAVIO_2=3;
+int TAMANHO_NAVIO_3=3;
+int TAMANHO_NAVIO_4=3;
 
 void inicializa_tabuleiro(int tabuleiro[TAMANHO_TABULEIRO_VERTICAL][TAMANHO_TABULEIRO_HORIZONTAL]) {
     for (int i = 0; i < TAMANHO_TABULEIRO_VERTICAL; i++) {
@@ -22,14 +24,56 @@ void inicializa_navio(int navio[], int tamanho) {
 }
 
 void posiciona_navio_vertical(int tabuleiro[TAMANHO_TABULEIRO_VERTICAL][TAMANHO_TABULEIRO_HORIZONTAL], int navio[], int tamanho, int linha, int coluna) {
+    if (tamanho + linha > TAMANHO_TABULEIRO_VERTICAL) {
+        printf("Erro: O navio não pode ser posicionado verticalmente nessa posição.\n");
+        return;
+    }
+
+    for (int i = 0; i < tamanho; i++) {
+        if (tabuleiro[linha + i][coluna] != AGUA) {
+            printf("Erro: O navio não pode ser posicionado verticalmente nessa posição, pois há um navio já presente.\n");
+            return;
+        }
+    }
+
     for (int i = 0; i < tamanho; i++) {
         tabuleiro[linha + i][coluna] = navio[i];
     }
 }
 
 void posiciona_navio_horizontal(int tabuleiro[TAMANHO_TABULEIRO_VERTICAL][TAMANHO_TABULEIRO_HORIZONTAL], int navio[], int tamanho, int linha, int coluna) {
+    if (tamanho + coluna > TAMANHO_TABULEIRO_HORIZONTAL) {
+        printf("Erro: O navio não pode ser posicionado horizontalmente nessa posição.\n");
+        return;
+    }
+
+    for (int i = 0; i < tamanho; i++) {
+        if (tabuleiro[linha][coluna + i] != AGUA) {
+            printf("Erro: O navio não pode ser posicionado horizontalmente nessa posição, pois há um navio já presente.\n");
+            return;
+        }
+    }
+
     for (int i = 0; i < tamanho; i++) {
         tabuleiro[linha][coluna + i] = navio[i];
+    }
+}
+
+void posiciona_navio_diagonal(int tabuleiro[TAMANHO_TABULEIRO_VERTICAL][TAMANHO_TABULEIRO_HORIZONTAL], int navio[], int tamanho, int linha, int coluna) {
+    if (tamanho + linha > TAMANHO_TABULEIRO_VERTICAL || tamanho + coluna > TAMANHO_TABULEIRO_HORIZONTAL) {
+        printf("Erro: O navio não pode ser posicionado diagonalmente nessa posição.\n");
+        return;
+    }
+
+    for (int i = 0; i < tamanho; i++) {
+        if (tabuleiro[linha + i][coluna + i] != AGUA) {
+            printf("Erro: O navio não pode ser posicionado diagonalmente nessa posição, pois há um navio já presente.\n");
+            return;
+        }
+    }
+
+    for (int i = 0; i < tamanho; i++) {
+        tabuleiro[linha + i][coluna + i] = navio[i];
     }
 }
 
@@ -52,24 +96,39 @@ int main() {
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
-    // Iniciando o tabuleiro com zeros (0 representa posição vazia, água)
     int tabuleiro[TAMANHO_TABULEIRO_VERTICAL][TAMANHO_TABULEIRO_HORIZONTAL];
     int navio1[TAMANHO_NAVIO_1]; // Navio vertical
     int navio2[TAMANHO_NAVIO_2]; // Navio horizontal
+    int navio3[TAMANHO_NAVIO_3]; // Navio diagonal
+    int navio4[TAMANHO_NAVIO_4]; // Navio diagonal
     
     inicializa_tabuleiro(tabuleiro);
     inicializa_navio(navio1, TAMANHO_NAVIO_1);
     inicializa_navio(navio2, TAMANHO_NAVIO_2);
+    inicializa_navio(navio3, TAMANHO_NAVIO_3);
+    inicializa_navio(navio4, TAMANHO_NAVIO_4);
 
     posiciona_navio_horizontal(tabuleiro, navio2, TAMANHO_NAVIO_2, 0, 0);
     posiciona_navio_vertical(tabuleiro, navio1, TAMANHO_NAVIO_1, 2, 1);
 
+    // posiciona navios errados
+    // Exemplos de navios posicionado fora dos limites do tabuleiro
+    // posiciona_navio_horizontal(tabuleiro, navio2, TAMANHO_NAVIO_2, 0, 8);
+    // posiciona_navio_vertical(tabuleiro, navio1, TAMANHO_NAVIO_1, 8, 1);
+    //posiciona_navio_diagonal(tabuleiro, navio3, TAMANHO_NAVIO_3, 8, 8);
+
+    printf("Tabuleiro após posicionamento dos navios iniciais:\n\n");
     exibe_tabuleiro(tabuleiro);
+    printf("\n------\n\n");
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
     // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    posiciona_navio_diagonal(tabuleiro, navio3, TAMANHO_NAVIO_3, 5, 5);
+    posiciona_navio_diagonal(tabuleiro, navio4, TAMANHO_NAVIO_4, 7, 2);
+    printf("Tabuleiro após posicionamento dos navios diagonais:\n\n");
+    exibe_tabuleiro(tabuleiro);
 
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
